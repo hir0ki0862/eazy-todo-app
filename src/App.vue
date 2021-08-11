445417c6764072f8acb4b2af0dae0cedff531831
soramaru <tatara0862@gmial.com>
2021-08-11 11:01:50 +0900

    1から作成し直し

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <template>
-  <div>
-    <myheader></myheader>
-    <addtodo></addtodo>
-    <todoarea></todoarea>
-  </div>
 </template>
 
 <script>
-import myheader from "@/components/myheader";
-import addtodo from "@/components/addtodo";
-import todoarea from "@/components/todoarea";
-
 export default {
-  name: "App",
-  components: {
-    myheader,
-    addtodo,
-    todoarea,
-  },
-};
+  name: 'App'
+}
 </script>
 <style>
-body {
-  background-color: #7d77a1;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,5 @@ import App from './App.vue'
 
 // css
 import './assets/css/reset.css'
-// font
-import './assets/css/font.css'
 
 createApp(App).mount('#app')
